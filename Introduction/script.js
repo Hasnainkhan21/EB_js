6a088a40046c7a2fa3bd34da6dd9201d23c78a24
naino <Hasnainkhan21>
2025-03-25 15:13:15 +0500

    intro to js

---
+++ Introduction/script.js
@@ -0,0 +1,102 @@
+// console.log("hello javaSciprt at eb")
+
+/*hoisting:
+accesiing variables before declaration  */
+// console.log("The value of a is : ", x);
+// var x = 20; undefined
+// let x = 20; => error
+// const x = 20; => error
+
+//task
+console.log("                  ");
+console.log("                  ");
+console.log("***          ***");
+console.log("***          ***");
+console.log("***          ***");
+console.log("***          ***");
+console.log("****************");
+console.log("***          ***");
+console.log("***          ***");
+console.log("***          ***");
+console.log("***          ***");
+
+console.log("                  ");
+console.log("                  ");
+
+console.log(" ************** ");
+console.log("***          ***");
+console.log("***          ***");
+console.log("***          ***");
+console.log("***          ***");
+console.log("****************");
+console.log("***          ***");
+console.log("***          ***");
+console.log("***          ***");
+console.log("***          ***");
+
+console.log("                  ");
+console.log("                  ");
+
+console.log("       **********");
+console.log("*****************");
+console.log("***");
+console.log("**");
+console.log("*****************");
+console.log("              ***");
+console.log("               **");
+console.log("*****************");
+console.log("*************    ");
+
+console.log("                  ");
+console.log("                  ");
+
+console.log("***            ***");
+console.log("****           ***");
+console.log("********       ***");
+console.log("***     **     ***");
+console.log("***       **   ***");
+console.log("***        *******");
+console.log("***          *****");
+console.log("***           ****");
+console.log("***            ***");
+
+console.log("                  ");
+console.log("                  ");
+
+console.log(" ************** ");
+console.log("***          ***");
+console.log("***          ***");
+console.log("***          ***");
+console.log("***          ***");
+console.log("****************");
+console.log("***          ***");
+console.log("***          ***");
+console.log("***          ***");
+console.log("***          ***");
+console.log("                  ");
+console.log("                  ");
+
+console.log("    ********");
+console.log("      ***");
+console.log("      ***");
+console.log("      ***");
+console.log("      ***");
+console.log("      ***");
+console.log("      ***");
+console.log("      ***");
+console.log("      ***");
+console.log("    ********");
+
+
+console.log("                  ");
+console.log("                  ");
+
+console.log("***            ***");
+console.log("****           ***");
+console.log("********       ***");
+console.log("***     **     ***");
+console.log("***       **   ***");
+console.log("***        *******");
+console.log("***          *****");
+console.log("***           ****");
+console.log("***            ***");
